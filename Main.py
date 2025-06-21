bb44c0b99aca14596702ac4ab59ffe9e58f67424
AltCodeWizarD <rusich2@gmail.com>
2025-06-22 02:51:03 +0300

    Изменил структуру
    Убрал из цикла наполнение справочника рун.скилов
    Сократил наполнение параметров персонажа в зависимости от выбора пола

--- Main.py
+++ Main.py
@@ -11,6 +11,50 @@ fake = Faker("ru_RU")
 gender = ['male', 'female']
 characteristics = range(3, 18)
 
+skills = [
+    'Стремительный прыжок',
+    'Электрический выстрел',
+    'Ледяной удар',
+    'Стремительный удар',
+    'Кислотный взгляд',
+    'Тайный побег',
+    'Ледяной выстрел',
+    'Огненный заряд'
+]
+
+letters = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+runic_skills = []
+for skill in skills:
+    runic = skill
+    for letter, replacement in letters.items():
+        runic = runic.replace(letter, replacement)
+    runic_skills.append(runic)
+
 def generate_character(number):
     file_number = number + 1
     filename = os.path.join("char_cards", f"result{file_number}.svg")
@@ -19,82 +63,29 @@ def generate_character(number):
     
     random_strength, random_agility, random_endurance, random_intelligence, random_luck = random.sample(characteristics, 5)
     
-    skills = [
-        'Стремительный прыжок',
-        'Электрический выстрел',
-        'Ледяной удар',
-        'Стремительный удар',
-        'Кислотный взгляд',
-        'Тайный побег',
-        'Ледяной выстрел',
-        'Огненный заряд'
-    ]
-    
-    letters = {
-        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
-        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
-        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
-        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
-        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
-        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
-        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
-        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
-        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
-        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
-        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
-        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
-        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
-        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
-        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
-        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
-        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
-        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
-        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
-        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
-        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
-        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
-        ' ': ' '
-    }
-
-    runic_skills = []
-    for skill in skills:
-        runic = skill
-        for letter, replacement in letters.items():
-            runic = runic.replace(letter, replacement)
-        runic_skills.append(runic)
-
     random_skill_1, random_skill_2, random_skill_3 = random.sample(runic_skills, 3)
 
     if random_gender == 'male':
-        context = {
-            "first_name": fake.first_name_male(),
-            "last_name": fake.last_name_male(),
-            "job": fake.job(),
-            "town": fake.city(),
-            "strength": random.choice(characteristics),
-            "agility": random.choice(characteristics),
-            "endurance": random.choice(characteristics),
-            "intelligence": random.choice(characteristics),
-            "luck": random.choice(characteristics),
-            "skill_1": random_skill_1,
-            "skill_2": random_skill_2,
-            "skill_3": random_skill_3,
-        }
+        first_name = fake.first_name_male()
+        last_name = fake.last_name_male()
     else:
-        context = {
-            "first_name": fake.first_name_female(),
-            "last_name": fake.last_name_female(),
-            "job": fake.job(),
-            "town": fake.city(),
-            "strength": random.choice(characteristics),
-            "agility": random.choice(characteristics),
-            "endurance": random.choice(characteristics),
-            "intelligence": random.choice(characteristics),
-            "luck": random.choice(characteristics),
-            "skill_1": random_skill_1,
-            "skill_2": random_skill_2,
-            "skill_3": random_skill_3,
-        }
+        first_name = fake.first_name_female()
+        last_name = fake.last_name_female()
+
+    context = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "job": fake.job(),
+        "town": fake.city(),
+        "strength": random_strength,
+        "agility": random_agility,
+        "endurance": random_endurance,
+        "intelligence": random_intelligence,
+        "luck": random_luck,
+        "skill_1": random_skill_1,
+        "skill_2": random_skill_2,
+        "skill_3": random_skill_3,
+    }
 
     file_operations.render_template("charsheet.svg", filename, context)
 
