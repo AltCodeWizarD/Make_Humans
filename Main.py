4015d22afa41d2c7165970682f34f6f4a778399e
AltCodeWizarD <rusich2@gmail.com>
2025-07-20 01:48:04 +0300

    Убрал длинные словари
    Убрал словарь рун. букв
    Убрал список скилов
    
    Все вынес в отдельные доки

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import os
 import random
 
 from faker import Faker
+from skills_dictionary import skills
+from letters_dictionary import letters
 import file_operations
 
 os.makedirs("char_cards", exist_ok=True)
@@ -11,43 +13,6 @@ fake = Faker("ru_RU")
 gender = ['male', 'female']
 characteristics = range(3, 18)
 
-skills = [
-    'Стремительный прыжок',
-    'Электрический выстрел',
-    'Ледяной удар',
-    'Стремительный удар',
-    'Кислотный взгляд',
-    'Тайный побег',
-    'Ледяной выстрел',
-    'Огненный заряд'
-]
-
-letters = {
-    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
-    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
-    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
-    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
-    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
-    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
-    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
-    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
-    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
-    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
-    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
-    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
-    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
-    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
-    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
-    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
-    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
-    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
-    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
-    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
-    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
-    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
-    ' ': ' '
-}
-
 runic_skills = []
 for skill in skills:
     runic = skill
@@ -55,15 +20,26 @@ for skill in skills:
         runic = runic.replace(letter, replacement)
     runic_skills.append(runic)
 
+
 def generate_character(number):
     file_number = number + 1
     filename = os.path.join("char_cards", f"result{file_number}.svg")
-    
+
     random_gender = random.choice(gender)
-    
-    random_strength, random_agility, random_endurance, random_intelligence, random_luck = random.sample(characteristics, 5)
-    
-    random_skill_1, random_skill_2, random_skill_3 = random.sample(runic_skills, 3)
+
+    (
+        random_strength,
+        random_agility,
+        random_endurance,
+        random_intelligence,
+        random_luck
+    ) = random.sample(characteristics, 5)
+
+    (
+        random_skill_1,
+        random_skill_2,
+        random_skill_3
+    ) = random.sample(runic_skills, 3)
 
     if random_gender == 'male':
         first_name = fake.first_name_male()
@@ -89,9 +65,11 @@ def generate_character(number):
 
     file_operations.render_template("charsheet.svg", filename, context)
 
+
 def main():
     for number in range(10):
         generate_character(number)
+
 
 if __name__ == '__main__':
     main()
