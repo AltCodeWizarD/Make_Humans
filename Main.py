17d23a8247ea5cb751e8afffca40c7c990a659cc
AltCodeWizarD <rusich2@gmail.com>
2025-07-21 00:24:29 +0300

    Исправления по структуре
    Заменил регистр глобальных переменных.

--- Main.py
+++ Main.py
@@ -2,14 +2,15 @@ import os
 import random
 
 from faker import Faker
+
 from skills_dictionary import skills
 from letters_dictionary import letters
 import file_operations
 
 fake = Faker("ru_RU")
-runic_skills = []
-gender = ['male', 'female']
-characteristics = range(3, 18)
+RUNIC_SKILLS = []
+GENDER = ['male', 'female']
+CHARACTERISTICS = range(3, 18)
 
 os.makedirs("char_cards", exist_ok=True)
 
@@ -17,14 +18,14 @@ for skill in skills:
     runic = skill
     for letter, replacement in letters.items():
         runic = runic.replace(letter, replacement)
-    runic_skills.append(runic)
+    RUNIC_SKILLS.append(runic)
 
 
 def generate_character(number):
     file_number = number + 1
     filename = os.path.join("char_cards", f"result{file_number}.svg")
 
-    random_gender = random.choice(gender)
+    random_gender = random.choice(GENDER)
 
     (
         random_strength,
@@ -32,26 +33,26 @@ def generate_character(number):
         random_endurance,
         random_intelligence,
         random_luck
-    ) = random.sample(characteristics, 5)
+    ) = random.sample(CHARACTERISTICS, 5)
 
     (
         random_skill_1,
         random_skill_2,
         random_skill_3
-    ) = random.sample(runic_skills, 3)
+    ) = random.sample(RUNIC_SKILLS, 3)
 
     if random_gender == 'male':
-        first_name = fake.first_name_male()
-        last_name = fake.last_name_male()
+        first_name = FAKE.first_name_male()
+        last_name = FAKE.last_name_male()
     else:
-        first_name = fake.first_name_female()
-        last_name = fake.last_name_female()
+        first_name = FAKE.first_name_female()
+        last_name = FAKE.last_name_female()
 
     context = {
         "first_name": first_name,
         "last_name": last_name,
-        "job": fake.job(),
-        "town": fake.city(),
+        "job": FAKE.job(),
+        "town": FAKE.city(),
         "strength": random_strength,
         "agility": random_agility,
         "endurance": random_endurance,
